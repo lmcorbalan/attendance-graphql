5a1435161b6e139c8c8b98e80f463613eb44b89f
Lisandro Corbalan <lmcorbalan@gmail.com>
2018-03-24 11:48:05 -0300

    Add Session mutations

--- src/api/resources/session/session.model.js
+++ src/api/resources/session/session.model.js
@@ -5,13 +5,21 @@ const sessionSchema = new mongoose.Schema({
     type: String,
     required: [true, "Session Place is required"]
   },
+
   date: {
     type: Date,
     required: [true, "Session Date is required"]
   },
+
   type: {
     type: String,
     required: [true, "Session Type is required"]
+  },
+
+  active: {
+    type: Boolean,
+    required: true,
+    default: true
   }
 })
 
--- src/api/resources/session/session.resolvers.js
+++ src/api/resources/session/session.resolvers.js
@@ -1,16 +1,36 @@
 import { Session } from './session.model'
 
 const getSession = (_, {id}, {user}) => {
-  return Session.findById(id).exec()
+  return Session.findOne({id: id, active: true}).exec()
 }
 
 const allSessions = (_, __, {user}) => {
-  return Session.find({}).exec()
+  return Session.find({active: true}).exec()
+}
+
+const newSession = (_, {input}) => {
+  return Session.create(input)
+}
+
+const updateSession = (_, {input}) => {
+  const {id, ...update} = input
+
+  return Session.findByIdAndUpdate(id, update, {new: true}).exec()
+}
+
+const removeSession = (_, {id}) => {
+  return Session.findByIdAndUpdate(id, {active: false}, {new: true}).exec()
 }
 
 export const sessionResolvers = {
   Query: {
     allSessions,
     Session: getSession
+  },
+
+  Mutation: {
+    newSession,
+    updateSession,
+    removeSession
   }
 }
